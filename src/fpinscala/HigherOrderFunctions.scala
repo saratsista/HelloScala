cd739c18cff5ed34ddb5d8f827df3f05375193e4
saratsista <saratsista@users.noreply.github.com>
2020-08-25 09:04:57 -0700

    HOF

--- src/fpinscala/HigherOrderFunctions.scala
+++ src/fpinscala/HigherOrderFunctions.scala
@@ -11,4 +11,31 @@ object HigherOrderFunctions {
 
     go(n-2, 0, 1)
   }
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int = {
+      if(n <= 0)  acc
+      else go(n-1, n*acc)
+
+    }
+
+    go(n, 1)
+  }
+
+  def abs(n: Int): Int = {
+    if(n < 0) -n
+    else n
+  }
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatResult("factorial", 4, factorial))
+    println(formatResult("nth fibonacci", 6, fibonacci))
+    println(formatResult("abs", -8, abs))
+  }
 }
